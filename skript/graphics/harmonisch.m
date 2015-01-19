b111fcb27bd82d741d2b37ae451777b189b53e7f
Andreas Mueller <andreas.mueller@othello.ch>
2015-01-20 00:34:40 +0100

    hermite poly

--- skript/graphics/harmonisch.m
+++ skript/graphics/harmonisch.m
@@ -3,10 +3,23 @@
 #
 # (c) 2014 Prof Dr Andreas Mueller, Hochschule Rapperswil
 #
-pkg load miscellaneous;
 
-for x = 0.01 * (-200:200)
-	hermitepoly(1, x)
+s = zeros(401,11);
+
+N = zeros(10);
+
+for n = 1:10
+	N(n) = 2^n * sqrt(factorial(n) / factorial(2 * n))
 endfor;
+
+for i = (-200:200)
+	x = i * 0.1
+	s(201 + i, 1) = x;
+	for n = 1:10
+		s(201 + i, 1 + n) = N(n) * hermitepoly(n, x) * exp(-(x / 2)^2);
+	endfor;
+endfor
+
+s
 
 
