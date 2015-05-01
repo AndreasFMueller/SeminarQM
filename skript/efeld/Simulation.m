aa16b535505e467b6da5a9cc678d1d26a66658bd
Michael Cerny <mcerny@gmx.ch>
2015-05-01 17:06:05 +0200

    Fortschritte in der Simulation

--- skript/efeld/Simulation.m
+++ skript/efeld/Simulation.m
@@ -48,87 +48,69 @@ end
 
 %-----Verarbeitung gestört-----
 gamma = 0;
-%epsilon = 5*10^-4;
-%epsilon = 10^3;
-epsilon = 5*10^2;
 epsilon = 4*10^3;
 E1_k = zeros(1, length(n));
 H1 = x;
-psiHPsi = zeros(1, length(x));
 PsiG = zeros(length(n), length(x));
 
-for ln = n
-psi1_l = zeros(1, length(x));
-
-E1_k(ln) = sum(~Psi(ln, :).*H1.*Psi(ln, :).*delta);
+% for ln = n
+% 	psi1_l = zeros(1, length(x));
+% 
+% 	E1_k(ln) = sum(~Psi(ln, :).*H1.*Psi(ln, :).*delta);
+% 
+% 	loopCount = 0;
+% 	k = ln;
+% 	if k > 1
+% 		for l = 1 : ln - 1
+% 			psi1_l = psi1_l + Psi(l, :).*H1.*Psi(k, :) ./ (E(k)-E(l));
+% 				loopCount = loopCount + 1;
+% 		end
+% 	else
+% 		psi1_l = psi1_l + Psi(k, :).*H1.*Psi(k, :) ./ (E(k));
+% 			loopCount = loopCount + 1;
+% 	end
+% end
 
 loopCount = 0;
-switch 2
-case 1
+for ln = n
+	E1_k(ln) = sum(~Psi(ln, :).*H1.*Psi(ln, :).*delta);
+	
+	psi1_l = zeros(1, length(x));
+
 	k = ln;
 	if k > 1
-		for l = 1 : ln - 1
-			psi1_l = psi1_l + Psi(l, :).*H1.*Psi(k, :) ./ (E(k)-E(l));
-				loopCount = loopCount + 1;
-		end
-	else
-		psi1_l = psi1_l + Psi(k, :).*H1.*Psi(k, :) ./ (E(k));
+		for l = 1 : k - 1
+%			psi0_l = sum(~Psi(l, :).*H1.*Psi(k, :).*delta) / (E(k)-E(l));
+			psi0_l = dot(Psi(l, :), H1.*Psi(k, :)) / (E(k)-E(l));
+			psi1_l = psi1_l + psi0_l .* Psi(l, :);
 			loopCount = loopCount + 1;
-	end
-
-case 2
-	k = ln;
-	if k > 1
-		for l = 1 : ln - 1
-			psiHPsiInt = 0;		% Variable zur Integration
-			for lx = 1 : length(x)		% integration von -l bis l
-				psiHPsi(lx) = psiHPsiInt;		% erster Wert ist 0 jeder weitere lx-1
-				psiHPsiInt = psiHPsiInt + ~Psi(l, lx)*x(lx)*Psi(k, lx)*delta;
-			end
-				psi1_l = psi1_l + psiHPsi ./ (E(k)-E(l));
-				loopCount = loopCount + 1;
 		end
 	else
-		psi1_l = psi1_l + Psi(k, :).*H1.*Psi(k, :) ./ (E(k));
-			loopCount = loopCount + 1;
-	end
-
-case 3
-	if ln > 1
-		for l = 1 : ln
-			for k = 1 : ln
-				if k ~= l
-					psi1_l = psi1_l + Psi(l, :).*H1.*Psi(k, :) ./ (E(k)-E(l));
-					loopCount = loopCount + 1;
-				end
-			end
-		end
-	else
-		psi1_l = psi1_l + Psi(1, :).*H1.*Psi(1, :) ./ (E(1));
+		psi0_l = sum(~Psi(1, :).*H1.*Psi(1, :).*delta) / (E(1)-0);
+		psi1_l = psi0_l .* Psi(1, :);
 		loopCount = loopCount + 1;
 	end
+	
+	figure
+	hold on;
+	plot(x, psi1_l(:), 'Color', 'green')
+	
+	PsiG(ln, 1:length(x)) = (1+1i*epsilon*gamma).*Psi(ln, 1:length(x)) ...
+													+ epsilon.*psi1_l;
 end
 
-PsiG(ln, 1:length(x)) = (1+1i*epsilon*gamma).*Psi(ln, 1:length(x)) ...
-												+ epsilon.*psi1_l;
-%PsiG(ln, 1:length(x)) = epsilon.*psi1_l;
-end
 
 %-----Plot grafik 1: Psi(x)-----
-colors = [1 0 0; 0 1 0; 0 0 1; 0 1 1; 1 0 1];
-clf('reset')			% clear figure
+%clf('reset')			% clear figure
 hold on;
 
 s = zeros(1, length(n));
 for ln = n		% ungestoerter Plot
-%	subplot(length(n), 1, ln)
-	plot(x, Psi(ln, 1:length(x)) + (ln-1)*2e05, 'Color', 'black')		% Psi
+	plot(x, Psi(ln, :) + (ln-1)*2e05, 'Color', 'black')		% Psi
 
-	s(ln) = sum(Psi(ln, 1:length(x)).^2.*delta);
+	s(ln) = sum(Psi(ln, :).^2.*delta);
 end
 
-% figure
-% hold on;
 % print('Psi_ungestoert', '-depsc', '-noui')
 % print('Psi_ungestoert', '-dpdf', '-noui')
 
@@ -137,18 +119,13 @@ end
 
 sG = zeros(1, length(n));
 for ln = n		% gestoerter Plot
-%	subplot(length(n), 1, ln)
-%	plot(x, PsiG(ln, 1:length(x)) +(ln-1)*2e05, 'Color', colors(ln, :))		% Psi
-	plot(x, PsiG(ln, 1:length(x)) +(ln-1)*2e05, 'Color', 'red')		% Psi
-%	plot(x, PsiG(ln, 1:length(x)), 'Color', color(ln, :))		% Psi
+	plot(x, PsiG(ln, 1:length(x)) + (ln-1)*2e05, 'Color', 'red')		% Psi
 
-	sG(ln) = sum(abs(PsiG(ln, 1:length(x))).^2.*delta);
+	sG(ln) = sum(PsiG(ln, :).^2.*delta);
 end
 
-%print('grafik_1_Psi', '-depsc', '-noui')
-%print('grafik_1_Psi', '-dpdf', '-noui')
 % print('Psi_gestoert', '-depsc', '-noui')
-print('Psi_gestoert', '-dpdf', '-noui')
+% print('Psi_gestoert', '-dpdf', '-noui')
 
 %-----Plot grafik 2: E(n, a)-----
 figure
